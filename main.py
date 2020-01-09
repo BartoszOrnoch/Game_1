123ef47170fe826c21299ef422f5df14c098ad53
Bartosz Ornoch <bartekorn@gmail.com>
2020-01-09 21:47:15 +0100

    dodano magazynek, niesmiertelnosc po staceniu zycia

--- main.py
+++ main.py
@@ -1,162 +1,123 @@
 import pygame
 import random
 import math
+import settings
+from ships import Ship, Enemy
 
 
 pygame.init()
-
-WIDTH = 800
-HEIGHT = 600
-SHIP_WIDTH = 64
-SHIP_HEIGHT = 64
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-title = pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load("ufooo.png")
-pygame.display.set_icon(icon)
-playership_img = pygame.image.load("space-invaders.png")
-enemy_img = pygame.image.load("outer-space-alien.png")
-bullet_img = pygame.image.load("favicon.png")
-
-
-playerX = WIDTH // 2 - SHIP_WIDTH // 2
-playerY = HEIGHT - (SHIP_HEIGHT + 10)
+screen = pygame.display.set_mode(
+    (settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-
-
-class Bullet():
-    
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX + 16
-        self.coordY = coordY + 16
-        self.deltaY = 4
-        self.hit = False
-    
-    def draw(self):
-        screen.blit(bullet_img, (self.coordX, self.coordY))
-    
-    def move(self):
-        self.coordY += self.deltaY
-
-class EnemyBullet(Bullet):
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX + 16
-        self.coordY = coordY + 16
-        self.deltaY = 4
-        self.hit = False  
-
-class Ship():
-
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
-        self.deltaX = 0
-        self.deltaY = 0
-        self.bullets = []
-    
-    def draw(self):
-        screen.blit(playership_img, (self.coordX, self.coordY))
-    
-    def update_coords(self):
-        if self.coordX > WIDTH - 64:
-            self.coordX = WIDTH - 64
-        elif self.coordX < 0:
-            self.coordX = 0
-        else:
-            self.coordX += self.deltaX
-    def shoot(self):
-        self.bullets.append(Bullet(self.coordX, self.coordY))
-
-class Enemy():
-
-    enemy_bullets = []
-    def __init__(self):
-        self.coordX = random.randint(0, WIDTH - 64)
-        self.coordY = random.randint(64, HEIGHT//2)
-        self.deltaX = 2
-        self.deltaY = 0
-        self.alive = True
-    
-    def draw(self):
-        screen.blit(enemy_img, (self.coordX, self.coordY))
-    
-    def move(self):
-        if self.coordX > WIDTH - 64:
-            self.coordX = WIDTH - 64
-            self.deltaX *= -1
-            self.coordY += 5
-        elif self.coordX < 0:
-            self.coordX = 0
-            self.deltaX *= -1
-            self.coordY += 5
-        else:
-            self.coordX += self.deltaX
-    
-    def shoot(self):
-        if random.randint(1, 200) == 1:
-            self.enemy_bullets.append(EnemyBullet(self.coordX, self.coordY))
-
-
-    
+title = pygame.display.set_caption("Space Invaders")
+pygame.display.set_icon(settings.GAME_ICON)
 
 
 def initialize_enemies(number_of_enemies):
-    return [Enemy() for i in range(number_of_enemies)]
+    return [Enemy(random.randint(0, settings.SCREEN_WIDTH-64), random.randint(0, 400), random.randint(1, 4)) for _ in range(number_of_enemies)]
 
-def detect_colision(bullet, alien):
-    alien_center_x = alien.coordX + 32
-    alien_center_y = alien.coordY + 32
-    bullet_center_x = bullet.coordX + 16
-    bullet_center_y = bullet.coordY + 16
-    distance = math.sqrt((alien_center_x - bullet_center_x)**2 + (alien_center_y - bullet_center_y)**2)
-    if distance < 32:
+
+def detect_colision(x1, y1, x2, y2, max_distance=32):
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    if distance < max_distance:
         return True
-        
+
+
+def draw(img, coord_tuple):
+    screen.blit(img, coord_tuple)
+
 
 def main():
+    time = 0
     running = True
-    playership = Ship(playerX, playerY)
+    playership = Ship(settings.PLAYER_BASE_X, settings.PLAYER_BASE_Y)
     enemies = initialize_enemies(3)
-
 
     while running:
 
-        screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    playership.deltaX = -4
-                if event.key == pygame.K_RIGHT:
-                    playership.deltaX = 4
                 if event.key == pygame.K_SPACE:
                     playership.shoot()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    playership.deltaX = 0
-        playership.update_coords()
-        playership.draw()
-        for bullet in playership.bullets:
-            for enemy in enemies:
-                if detect_colision(bullet, enemy):
+                if event.key == pygame.K_r:
+                    playership.ammo = 0
+                    playership.reloading = True
+                    reload_time = time
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                playership.set_direction_X('left')
+            if keys[pygame.K_RIGHT]:
+                playership.set_direction_X('right')
+            if keys[pygame.K_UP]:
+                playership.set_direction_Y('up')
+            if keys[pygame.K_DOWN]:
+                playership.set_direction_Y('down')
+            if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
+                playership.set_direction_X('stop')
+            if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+                playership.set_direction_X('stop')
+            if keys[pygame.K_UP] and keys[pygame.K_DOWN]:
+                playership.set_direction_Y('stop')
+            if not keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+                playership.set_direction_Y('stop')
+        enemies[:] = [enemy for enemy in enemies if enemy.alive]
+        Ship.bullets[:] = [
+            bullet for bullet in Ship.bullets if bullet.coordY > 0 and not bullet.hit]
+        Enemy.bullets[:] = [bullet for bullet in Enemy.bullets if bullet.coordY <
+                            settings.SCREEN_HEIGHT and not bullet.hit]
+
+        screen.fill((255, 255, 255))
+
+        for enemy in enemies:
+            if detect_colision(*enemy.give_position(1), *playership.give_position(1), max_distance=64) and not playership.invincible:
+                enemy.alive = False
+                playership.reduce_lifes()
+                invincible_time = time
+                continue
+            for bullet in playership.bullets:
+                if bullet.hit:
+                    continue
+                if detect_colision(*enemy.give_position(1), *bullet.give_position(1)):
                     enemy.alive = False
                     bullet.hit = True
                     break
-        enemies[:] = [enemy for enemy in enemies if enemy.alive]
-        playership.bullets[:] = [bullet for bullet in playership.bullets if bullet.coordY > 0 and not bullet.hit]
-        for enemy in enemies:
-            enemy.move()
-            enemy.draw()
-            enemy.shoot()
-        for ebulet in Enemy.enemy_bullets:
-            ebulet.move()
-            ebulet.draw()
-        for bulet in playership.bullets:
-            bulet.move()
-            bulet.draw()
+            else:
+                draw(enemy.img, enemy.give_position(0))
+                enemy.move()
+                if random.randint(1, 100) == 100:
+                    enemy.shoot()
+
+        for bullet in Ship.bullets:
+            draw(bullet.img, bullet.give_position(0))
+            bullet.move()
+
+        for bullet in Enemy.bullets:
+            if detect_colision(*bullet.give_position(1), *playership.give_position(1)) and not playership.invincible:
+                playership.reduce_lifes()
+                bullet.hit = True
+                invincible_time = time
+            else:
+                draw(bullet.img, bullet.give_position(0))
+                bullet.move()
+
+        if playership.reloading:
+            if time-3000 > reload_time:
+                playership.reload()
+
+        if playership.invincible:
+            draw(Enemy.img, playership.give_position(0))
+            if time - 3000 > invincible_time:
+                playership.invincible = False
+
+        if playership.lifes > 0:
+            playership.move()
+            draw(playership.img, playership.give_position(0))
+
         pygame.display.update()
-        clock.tick(60)
+        dt = clock.tick(60)
+        time += dt
+
 
 main()
--- ships.py
+++ ships.py
@@ -1,59 +1,115 @@
+import settings
+
+
 class Bullet():
-    
+
+    img = settings.BULLET_IMG
+    direction = -1
+
     def __init__(self, coordX, coordY, deltaY, deltaX=0):
-        self.coordX = coordX + 16
-        self.coordY = coordY + 16
+        self.coordX = coordX + settings.BULLET_WIDTH // 2
+        self.coordY = coordY + settings.BULLET_HEIGHT // 2
         self.deltaX = deltaX
-        self.deltaY = deltaY
+        self.deltaY = deltaY * self.direction
         self.hit = False
-    
-    def give_position(self):
-        return (self.coordX, self.coordY)
-    
+
+    # 0 - left, bottom corner, 1 - center
+    def give_position(self, position):
+        if position == 0:
+            return (self.coordX, self.coordY)
+        if position == 1:
+            return (self.coordX + settings.BULLET_WIDTH // 2, self.coordY + settings.BULLET_WIDTH // 2)
+
     def move(self):
         self.coordY += self.deltaY
-    
-    def give_center(self):
-        return (self.coordX + 16, self.coordY + 16)
+
+
+class EnemyBullet(Bullet):
+
+    direction = 1
+    img = settings.ENEMY_BULLET_IMG
+
 
 class Ship():
-    direction_of_bullets = -1
+
     bullets = []
+    img = settings.SHIP_IMG
+
     def __init__(self, coordX, coordY, deltaX=0, deltaY=0):
         self.coordX = coordX
         self.coordY = coordY
         self.deltaX = deltaX
         self.deltaY = deltaY
-    
-    def give_center(self):
-        return (self.coordX + 32, self.coordY + 32)
-    
-    def give_position(self):
-        return (self.coordX, self.coordY)
-    
-    def move(self, width):
-        if self.coordX > width - 64:
-            self.coordX = width - 64
-        elif self.coordX < 0:
-            self.coordX = 0
-        else:
-            self.coordX += self.deltaX
-            self.coordY += self.deltaY
+        self.lifes = 3
+        self.alive = True
+        self.reloading = False
+        self.ammo = 6
+        self.invincible = False
 
-    def shoot(self, speed=7):
-        Ship.bullets.append(Bullet(self.coordX, self.coordY, speed*Ship.direction_of_bullets))
+    def reduce_lifes(self):
+        if self.lifes > 0:
+            self.lifes -= 1
+            self.coordX = settings.PLAYER_BASE_X
+            self.coordY = settings.PLAYER_BASE_Y
+            self.invincible = True
+        else:
+            print('game_over')
+
+    def give_position(self, position):
+        if position == 0:
+            return (self.coordX, self.coordY)
+        if position == 1:
+            return (self.coordX + settings.SHIP_WIDTH // 2, self.coordY + settings.SHIP_HEIGHT // 2)
+
+    def move(self):
+        if self.coordX > settings.SCREEN_WIDTH - settings.SHIP_WIDTH and self.deltaX > 0:
+            self.deltaX = 0
+        elif self.coordX < 0 and self.deltaX < 0:
+            self.deltaX = 0
+        self.coordX += self.deltaX
+
+        if self.coordY < 0 and self.deltaY < 0:
+            self.deltaY = 0
+        elif self.coordY > settings.SCREEN_HEIGHT - settings.SHIP_HEIGHT and self.deltaY > 0:
+            self.deltaY = 0
+        self.coordY += self.deltaY
+
+    def set_direction_X(self, direction):
+        if direction == 'left':
+            self.deltaX = settings.DEFAULT_SHIP_DELTA_X * -1
+        elif direction == 'right':
+            self.deltaX = settings.DEFAULT_SHIP_DELTA_X
+        elif direction == 'stop':
+            self.deltaX = 0
+
+    def set_direction_Y(self, direction):
+        if direction == 'up':
+            self.deltaY = settings.DEFAULT_SHIP_DELTA_Y * -1
+        elif direction == 'down':
+            self.deltaY = settings.DEFAULT_SHIP_DELTA_Y
+        elif direction == 'stop':
+            self.deltaY = 0
+
+    def shoot(self, speed=settings.DEFAULT_SHIP_BULLET_SPEED):
+        if self.ammo > 0:
+            Ship.bullets.append(Bullet(self.coordX, self.coordY, speed))
+            self.ammo -= 1
+
+    def reload(self):
+        self.ammo = 6
+        self.reloading = False
 
 
 class Enemy(Ship):
-    direction_of_bullets = 1
     bullets = []
+    img = settings.ENEMY_IMG
+
     def __init__(self, coordX, coordY, deltaX=0, deltaY=0):
         super().__init__(coordX, coordY, deltaX, deltaY)
-        self.alive = True
-    
-    def move(self, width):
-        if self.coordX > width - 64:
-            self.coordX = width - 64
+
+    def move(self):
+        if self.coordX > settings.SCREEN_WIDTH - settings.ENEMY_WIDTH:
+            self.coordX = settings.SCREEN_WIDTH - settings.ENEMY_WIDTH
             self.deltaX *= -1
             self.coordY += 5
         elif self.coordX < 0:
@@ -62,7 +118,6 @@ class Enemy(Ship):
             self.coordY += 5
         else:
             self.coordX += self.deltaX
-            
-    
-    def shoot(self, speed=7):
-        Enemy.bullets.append(Bullet(self.coordX, self.coordY, speed*Enemy.direction_of_bullets))
+
+    def shoot(self, speed=settings.DEFAULT_ENEMY_BULLET_SPEED):
+        Enemy.bullets.append(EnemyBullet(self.coordX, self.coordY, speed))
